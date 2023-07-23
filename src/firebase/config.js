0d5a30b0e3ae12458dd80538042aec2d05ff2f5a
RaniSu <109942160+RaniSu@users.noreply.github.com>
2023-07-23 15:48:53 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,16 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore'; 
 import 'firebase/compat/auth'; 
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDjQWmykFd4mCQYJEc4GVek4wX9YtOBWSs",
-  authDomain: "myvideos-63d5e.firebaseapp.com",
-  projectId: "myvideos-63d5e",
-  storageBucket: "myvideos-63d5e.appspot.com",
-  messagingSenderId: "155894143488",
-  appId: "1:155894143488:web:52b50c3d21cf6c524156e5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
